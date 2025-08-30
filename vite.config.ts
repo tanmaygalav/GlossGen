0e9631c37c051bc025370ac5090f3166ae35eab9
Tanmay Galav <tanmaygalav@gmail.com>
2025-08-30 15:36:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,18 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    root: '.', // folder containing index.html
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
+        '@': path.resolve(__dirname, './src'),
+      },
     },
     build: {
-      outDir: 'dist', // the folder that will be deployed
-      emptyOutDir: true, // clean it before build
-    }
+      outDir: 'dist', // where Netlify will serve files from
+      emptyOutDir: true,
+    },
   };
 });
