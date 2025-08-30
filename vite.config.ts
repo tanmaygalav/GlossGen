686814e7c4129ed2b8aa8a200c2cb0e594f117c5
Tanmay Galav <tanmaygalav@gmail.com>
2025-08-30 15:45:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     build: {
-      outDir: 'dist', // where Netlify will serve files from
+      outDir: 'dist',
       emptyOutDir: true,
     },
   };
