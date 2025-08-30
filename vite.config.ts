8af83defddee2491e70c684d5bcbedfcf60ee4e5
Tanmay Galav <tanmaygalav@gmail.com>
2025-08-30 15:33:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,22 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    root: '.', // folder containing index.html
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-    };
+    },
+    build: {
+      outDir: 'dist', // the folder that will be deployed
+      emptyOutDir: true, // clean it before build
+    }
+  };
 });
